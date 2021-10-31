1556037dc2a4e75501f8ccb43451b5d36aa08e96
Jakub Iwaszek <kubaa771@gmail.com>
2021-10-31 17:52:28 +0100

    finished ex 16

---
+++ 03ControlStructures/e16.py
@@ -0,0 +1,6 @@
+firstNumber = int(input("Enter first number: "))
+secondNumber = int(input("Enter second number: "))
+tab = [firstNumber, secondNumber]
+sortedTab = sorted(tab)
+
+print("Numbers in ascending order: ", sortedTab)
